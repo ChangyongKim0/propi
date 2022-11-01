d762d464713f131562c56f8d4876e9bfc3882b2e
ChangyongKim0 <cyklone001@gmail.com>
2022-11-01 00:03:18 +0000

    path updated

--- src/shortcut.js
+++ src/shortcut.js
@@ -1,7 +1,20 @@
 const API_URI = "http://beta.propi.moohae.net:81/api/";
 
-const IP_PORT = "http://propi.moohae.net:81";
+const API_URI_PY = "http://beta.propi.moohae.net:82/api/";
 
-const SERVICE_URI = "https://propi.moohae.net/";
+const IP_PORT = "http://beta.propi.moohae.net:81";
 
-module.exports = { API_URI, IP_PORT, SERVICE_URI };
+const SERVICE_URI = "https://beta.propi.moohae.net/";
+
+const CONFIG_PATH = "/home/CY/Developing/propi_back/config/";
+
+const DATA_PATH = "/home/CY/Developing/propi_back/data/";
+
+module.exports = {
+  API_URI,
+  API_URI_PY,
+  IP_PORT,
+  SERVICE_URI,
+  CONFIG_PATH,
+  DATA_PATH,
+};
